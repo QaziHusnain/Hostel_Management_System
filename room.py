21b36838b2470c3ca164bd3578655534f5920db9
Qazi <qazi.husnain878@gmail.com>
2023-12-30 22:01:55 -0800

    Room Management completed

--- room.py
+++ room.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from tkinter import *
 from tkinter import Tk
 from tkinter import ttk
@@ -11,12 +12,13 @@ class Room:
     def __init__ (self,root):
 
         self.root= root
+
         self.root.title("Manage Room")
-        self.root.geometry("1100x615+250+82")
+        self.root.geometry("1600x700+200+50")
         self.root.resizable(False,False)
         self.root["bg"] = "cadet blue"
 
-   
+
 
         toplabel=Label(self.root, text="Manage Room",font=("Time New Romen",22,"bold"), bg="black",fg="gold").pack(fill=X)
 
@@ -24,54 +26,67 @@ class Room:
 
         #label2= Label(frame2, text= "Register Staff", font=("Time New Romen",16,"bold"),bg="#2F4F4F", fg="white").place(x=110,y= 60)
         id_value = IntVar()
-        label_id = Label(self.root, text="ID", font=("Time New Romen", 12, "bold"), bg="cadet blue", fg="white").place(x=20,y=50)
+        self.id_value = id_value  # Define as instance variable
+        label_id = Label(self.root, text="ID", font=("Time New Romen", 12, "bold"), bg="cadet blue", fg="white").place(x=20, y=50)
         id_entry = Entry(self.root, width=30, textvariable=id_value).place(x=160, y=55)
 
         name = StringVar()
+        self.name = name
         label_name = Label(self.root, text="Name", font=("Time New Romen", 12, "bold"), bg="cadet blue", fg="white").place(x=20, y=100)
         name_entry = Entry(self.root, width=30, textvariable=name).place(x=160, y=105)
+
         fname = StringVar()
+        self.fname = fname  # Change this line
         label_fname = Label(self.root, text="Father Name", font=("Time New Romen", 12, "bold"), bg="cadet blue",fg="white").place(x=20, y=150)
         fname_entry = Entry(self.root, width=30, textvariable=fname).place(x=160, y=155)
         addres = StringVar()
+        self.addres=addres
         label_address = Label(self.root, text="Address", font=("Time New Romen", 12, "bold"), bg="cadet blue",fg="white").place(x=20, y=200)
         address_entry = Entry(self.root, width=30, textvariable=addres).place(x=160, y=205)
         DOB= StringVar()
+        self.DOB=DOB
         label_dob = Label(self.root, text="Date of Birth", font=("Time New Romen", 12, "bold"), bg="cadet blue",fg="white").place(x=20, y=250)
         dob_entry = Entry(self.root, width=30, textvariable=DOB).place(x=160, y=255)
         age = StringVar()
+        self.age=age
         label_age = Label(self.root, text="Age", font=("Time New Romen", 12, "bold"), bg="cadet blue", fg="white").place(x=20,y=300)
         age_entry = Entry(self.root, width=30, textvariable=age).place(x=160, y=305)
         contact = StringVar()
+        self.contact=contact
         label_contact = Label(self.root, text="Contact Number", font=("Time New Romen", 12, "bold"), bg="cadet blue",fg="white").place(x=20, y=350)
         contact_entry = Entry(self.root, width=30, textvariable=contact).place(x=160, y=355)
         roomn = StringVar()
+        self.roomn=roomn
         label_room_no = Label(self.root, text="Room No", font=("Time New Romen", 12, "bold"), bg="cadet blue",fg="white").place(x=20, y=400)
         room_no_entry = Entry(self.root, width=30, textvariable=roomn).place(x=160, y=405)
         fee = StringVar()
+        self.fee=fee
         label_room_fee = Label(self.root, text="Room Fee", font=("Time New Romen", 12, "bold"), bg="cadet blue",fg="white").place(x=20, y=455)
         room_fee_entry = Entry(self.root, width=30, textvariable=fee).place(x=160, y=455)
 
-       
+
          #=========== frame for separation ==========
         sep_frame = Frame(self.root, bd=5, width=4, height=500, bg="black")
         sep_frame.place(x=360, y=60)
 
 
-#===========Button Frame and Buttons=================       
+#===========Button Frame and Buttons=================
         #btn_frame=Frame (self.root,width=750,height=207,bg="cadet blue",bd=4, relief=RIDGE ).place(x=350,y=38)
 
-        btn1 = Button(self.root, text="Allocate Room", bg="black", fg="gold", bd=2, width=14,
+        btn1 = Button(self.root, text="Allocate Room",command=self.add_room, bg="black", fg="gold", bd=2, width=14,
                             font=("time new romens", 14, "bold"))
         btn1.place(x=400, y=48)
-        
+
         btn1 = Button(self.root, text="Remove Allocation", command=self.remove, bg="black", fg="gold", bd=2, width=16,
                             font=("time new romens", 14, "bold"))
         btn1.place(x=635, y=48)
-        
+
         btn1 = Button(self.root, text="Add New Room", command =self.open, bg="black", fg="gold", bd=2, width=14,
                             font=("time new romens", 14, "bold"))
         btn1.place(x=870, y=48)
+        btn_insert_student = Button(self.root, text="Insert Student", command=self.insert_student, bg="black",
+                                    fg="gold", bd=2, width=14, font=("time new romens", 14, "bold"))
+        btn_insert_student.place(x=1100, y=48)
 
 
 # =============================frame for tree==========================
@@ -119,6 +134,7 @@ class Room:
         self.room_detail.column("Room Fee", anchor=tk.CENTER, width=120)
 
         self.room_detail.pack(fill = BOTH, expand=1)
+        self.fetch_data()
     def open(self):
 
         addroom_window = Toplevel(self.root)
@@ -134,11 +150,14 @@ class Room:
         selected_item = self.room_detail.selection()
         if selected_item:
             record_id = self.room_detail.item(selected_item)['values'][0]
+            print(f"Deleting record with ID: {record_id}")
             query = "DELETE FROM Allocation WHERE id=%s"
-            my_connection(query, (record_id,))
+            my_connection.execute(query, (record_id,))
+            connection.commit()
             # refresh_data()
         else:
             messagebox.showinfo("Please", "select a record to delete")
+        self.fetch_data()
 
     def add_room(self):
 
@@ -146,7 +165,11 @@ class Room:
         name = self.name.get()
         fname = self.fname.get()
         address = self.addres.get()
-        DOB = self.DOB.get()
+        try:
+            DOB = datetime.strptime(self.DOB.get(), "%Y-%m-%d").date()
+        except ValueError:
+            messagebox.showerror("Error", "Invalid date format. Please use YYYY-MM-DD.")
+            return
         age = self.age.get()
         contact = self.contact.get()
         roomn = self.roomn.get()
@@ -173,8 +196,105 @@ class Room:
             # Close the cursor and connection
             my_connection.close()
             connection.close()
+        self.fetch_data()
+
+    def fetch_data(self):
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="1234",
+            database="hostel"
+        )
+        cursor = conn.cursor()
+        query = "SELECT * FROM Allocation"
+        cursor.execute(query)
+
+        # Fetch all records
+        rows = cursor.fetchall()
+
+        # Clear existing data in the treeview
+        for item in self.room_detail.get_children():
+            self.room_detail.delete(item)
+
+        # Insert new data into the treeview
+        for row in rows:
+            self.room_detail.insert("", tk.END, values=row)
+
+        # Close the connection
+        conn.close()
+
+    def insert_student(self):
+        # Create a new window to display student information
+        student_window = Toplevel(self.root)
+        student_window.title("Insert Student")
+        student_window.geometry("800x500")
+
+        # Create a Treeview to display student information
+        student_tree = ttk.Treeview(student_window, columns=(
+            "StdID", "Name", "Father Name", "Contact", "Address", "Class", "Fee"),
+                                    show="headings")
+
+        scroll_x = Scrollbar(student_tree, orient=HORIZONTAL)
+        scroll_y = Scrollbar(student_tree, orient=VERTICAL)
+
+        student_tree.bind("<<TreeviewSelect>>",
+                          lambda event: self.on_student_select(event, student_tree, student_window))
+
+        scroll_x.config(command=student_tree.xview)
+        scroll_y.config(command=student_tree.yview)
+
+        scroll_x.pack(side=BOTTOM, fill=X)
+        scroll_y.pack(side=RIGHT, fill=Y)
+
+        student_tree.heading("StdID", text="ID")
+        student_tree.heading("Name", text="Name")
+        student_tree.heading("Father Name", text="Father Name")
+        student_tree.heading("Contact", text="Contact")
+        student_tree.heading("Address", text="Address")
+        student_tree.heading("Class", text="Class")
+        student_tree.heading("Fee", text="Fee")
+
+        student_tree["show"] = "headings"
+
+        student_tree.column("StdID", anchor=tk.CENTER, width=120)
+        student_tree.column("Name", anchor=tk.CENTER, width=120)
+        student_tree.column("Father Name", anchor=tk.CENTER, width=120)
+        student_tree.column("Contact", anchor=tk.CENTER, width=120)
+        student_tree.column("Address", anchor=tk.CENTER, width=120)
+        student_tree.column("Class", anchor=tk.CENTER, width=120)
+        student_tree.column("Fee", anchor=tk.CENTER, width=120)
+
+        student_tree.pack(fill=tk.BOTH, expand=1)
+
+        # Fetch data from the Student table and insert into the Treeview
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="1234",
+            database="hostel"
+        )
+        cursor = conn.cursor()
+        cursor.execute("SELECT ID, Name, FName, Contact, Address, Class, Fee FROM Student")
+        students = cursor.fetchall()
+        for student in students:
+            student_tree.insert("", "end", values=student)
+
+    def on_student_select(self, event, treeview, window):
+        # Get the selected student's information
+        selected_item = treeview.selection()
+        if selected_item:
+            student_info = treeview.item(selected_item, "values")
+
+            # Populate the entry fields in the Manage Room window
+            self.id_value.set(student_info[0])
+            self.name.set(student_info[1])
+            self.fname.set(student_info[2])
+            self.contact.set(student_info[3])
+            self.addres.set(student_info[4])
 
 
+            # Close the student window
+            window.destroy()
 if __name__ == "__main__":
     root=Tk()
     obj= Room(root)
